eb4e0b03d3e23375b7cda162fbd05c4321c8b222
chatton <github.qpeyb@simplelogin.fr>
2023-08-22 10:14:37 +0100

    adding cast

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/chatton/go-mod-test
 
+require github.com/spf13/cast v1.5.1
+
 go 1.20
--- pkg/pagage.go
+++ pkg/pagage.go
@@ -1,3 +1,9 @@
 package pkg
 
+import "github.com/spf13/cast"
+
 const Version = "1.0.0"
+
+func Foo() {
+	cast.StringToDate("2020-01-01")
+}
